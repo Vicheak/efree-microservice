78422016eff32578be00232cbdf57ef0b5f69f76
Vicheak <suonvicheak991@gmail.com>
2024-09-16 17:53:55 +0700

    init project

---
+++ subscription/src/main/java/com/efree/subscription/api/SubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.efree.subscription.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionApplication.class, args);
+	}
+
+}
---
+++ subscription/src/test/java/com/efree/subscription/api/SubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efree.subscription.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
