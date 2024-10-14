db09be6935398e499713de20ba8df3983b4b7054
Vicheak <suonvicheak991@gmail.com>
2024-10-14 19:08:53 +0700

    update micro config

--- payment/src/main/java/com/efree/payment/api/PaymentApplication.java
+++ payment/src/main/java/com/efree/payment/api/PaymentApplication.java
@@ -2,7 +2,11 @@ package com.efree.payment.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@RefreshScope
 @SpringBootApplication
 public class PaymentApplication {
 
